57a27824a66599ef844115bb7c0f225b13aa384c
Snowy <snow2code@protonmail.com>
2025-08-15 01:20:08 +0100

    ex a

---
+++ index.pck
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:902e1b952c9e76a9f3422613f7b999584f3ccc39dfbc6728783b8bb66cdf1042
+size 116321632
---
+++ test.bat
@@ -0,0 +1,9 @@
+git lfs install
+
+git lfs track "*.pck"
+
+git add .gitattributes
+
+git add index.pck
+git commit -m "Add large file with Git LFS"
+git push origin main
